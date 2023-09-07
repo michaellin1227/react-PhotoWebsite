3f21426be508c1ac234007b7a186d1f03829246a
michael <michaellin5027@gmail.com>
2023-09-07 12:24:16 +0800

    index router

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import Nav from './components/Nav';
 import Footer from './components/Footer';
 import Homepage from './pages/Homepage';
 import About from './pages/About';
-import { Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import "./styles/style.css";
 
 function App() {
+
   return (
     <div className="App">
       <Nav />
       <Routes>
         <Route path='/' element={<Homepage />} />
         <Route path='/about' element={<About />} />
+        {/* 添加 index 路由 */}
+        <Route path='*' element={<Navigate to="/" />} />
       </Routes>
       <Footer />
     </div>
